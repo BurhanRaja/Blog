527882a03f894f4c18b9575fdf5b87b008f6e228
BurhanuddinRaja <mails4burhan@gmail.com>
2022-05-08 10:45:09 +0530

    static root added

--- mindblogging/settings.py
+++ mindblogging/settings.py
@@ -123,10 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-# Added Manually
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR , "static")
-]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 # Managing Media
 MEDIA_URL = '/media/'
