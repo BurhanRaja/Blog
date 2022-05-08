a07226757c4477283796f6f500355b95b867b39c
BurhanuddinRaja <mails4burhan@gmail.com>
2022-05-08 12:05:47 +0530

    updates

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,3 @@
-from django.http import HttpResponse
 from django.shortcuts import redirect, render
 from django.contrib.auth import authenticate
 from django.contrib.auth import logout as auth_logout
--- mindblogging/settings.py
+++ mindblogging/settings.py
@@ -21,9 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-from decouple import config
-
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
